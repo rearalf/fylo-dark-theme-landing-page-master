b7e446b54f2a773971847efe045033d46dc93683
rearalf <rear_recinos@hotmail.com>
2020-08-28 18:08:59 -0600

    Basic Settings Ready

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { render } = require('react-dom');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.[hash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	resolve: {
+		extensions: [ '.js', '.jsx' ],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+			{
+				test: /\.(s[ac]ss)$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					/* 'style-loader', */
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpe?g|svg)$/i,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'assets/img',
+					name: '[name].[ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html',
+			favicon: './src/images/favicon-32x32.png',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].css',
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+		open: true,
+	},
+};
