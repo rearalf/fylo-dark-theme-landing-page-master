3d496ff5a21546bad43905255a549b5ec9325891
rearalf <rear_recinos@hotmail.com>
2020-09-06 19:24:23 -0600

    All ready

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,6 +4,7 @@ import Logo from '../../images/logo.svg';
 import IconEmail from '../../images/icon-email.svg';
 import IconLocation from '../../images/icon-location.svg';
 import IconPhone from '../../images/icon-phone.svg';
+import { FaTwitter, FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 export const Footer = () => {
 	return (
@@ -17,6 +18,8 @@ export const Footer = () => {
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
 						</p>
 					</li>
+				</ul>
+				<ul className="list_footer">
 					<li className="list">
 						<img src={IconPhone} alt="Icon Phone" />
 						<p className="text"> +1-543-123-4567</p>
@@ -65,13 +68,30 @@ export const Footer = () => {
 						</a>
 					</li>
 				</ul>
+				<ul className="list_footer">
+					<li className="list">
+						<a href="https://twitter.com/ricardoalfar" target="_blank">
+							<FaFacebookF />
+						</a>
+					</li>
+					<li className="list">
+						<a href="https://twitter.com/ricardoalfar" target="_blank">
+							<FaTwitter />
+						</a>
+					</li>
+					<li className="list">
+						<a href="https://twitter.com/ricardoalfar" target="_blank">
+							<FaInstagram />
+						</a>
+					</li>
+				</ul>
 			</div>
-			<div>
-				<p className="attribution">
-					Challenge by
+			<div className="end_footer">
+				<p className="text">
+					Challenge by{' '}
 					<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
 						Frontend Mentor
-					</a>. Coded by
+					</a>. Coded by{' '}
 					<a href="https://github.com/rearalf" target="_blank">
 						Your Name Here
 					</a>.
